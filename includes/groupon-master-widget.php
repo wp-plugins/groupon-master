f491ccfd39f377ad424b17a90d0ec44563d39d07
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 13:59:52 +0000

    version 4.1
    git-svn-id: https://plugins.svn.wordpress.org/groupon-master/tags/4.1@821117 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/groupon-master-widget.php
@@ -0,0 +1,98 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'groupon_master_widget' );
+//Register Widget
+function groupon_master_widget() {
+register_widget( 'groupon_master_widget' );
+}
+
+class groupon_master_widget extends WP_Widget {
+	function groupon_master_widget() {
+	$widget_ops = array( 'classname' => 'Groupon Master', 'description' => __('Easy to use bombastic plugin that will be a great source of income for any wordpress webmaster. Groupon Master. ', 'groupon_master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'groupon_master_widget' );
+	$this->WP_Widget( 'groupon_master_widget', __('Groupon Master', 'groupon_master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		//Our variables from the widget settings.
+		$name = "Groupon Master";
+		$title = isset( $instance['title'] ) ? $instance['title'] :false;
+		$grouponspacer ="'";
+		$show_groupondeals = isset( $instance['show_groupondeals'] ) ? $instance['show_groupondeals'] :false;
+		$groupondeals_code = $instance['groupondeals_code'];
+		echo $before_widget;
+		
+		// Display the Widget Title
+	if ( $title )
+		echo $before_title . $name . $after_title;
+		//Display Groupon Deals
+	if ( $show_groupondeals )
+		echo $groupondeals_code;
+	echo $after_widget;
+	}
+		//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['down_link_groupon'] = $new_instance['down_link_groupon'];
+		update_option('down_link_groupon', $new_instance['down_link_groupon']);
+		$instance['name'] = strip_tags( $new_instance['name'] );
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['show_groupondeals'] = $new_instance['show_groupondeals'];
+		$instance['groupondeals_code'] = $new_instance['groupondeals_code'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'name' => __('Groupon Master', 'groupon_master'), 'title' => true, 'show_groupondeals' => false, 'groupondeals_code' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<br>
+		<b>Check the buttons to be displayed:</b>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['title'], true ); ?> id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><b><?php _e('Display Widget Title', 'groupon_master'); ?></b></label>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_groupondeals'], true ); ?> id="<?php echo $this->get_field_id( 'show_groupondeals' ); ?>" name="<?php echo $this->get_field_name( 'show_groupondeals' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_groupondeals' ); ?>"><b><?php _e('Display Groupon Deals', 'groupon_master'); ?></b></label>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'groupondeals_code' ); ?>"><?php _e('insert Groupon Deals Code:', 'groupon_master'); ?></label></br>
+	<textarea cols="35" rows="5" id="<?php echo $this->get_field_id( 'groupondeals_code' ); ?>" name="<?php echo $this->get_field_name( 'groupondeals_code' ); ?>" ><?php echo stripslashes ($instance['groupondeals_code']); ?></textarea>
+	</p>
+	<div class="description">Copy and Paste your groupon deals script code from groupon website.</div>
+	<br>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<p>
+		<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+		&nbsp;
+		<b>Shortcode Framework</b>
+		</p>
+		<div class="description">The shortcode framework allows you to insert Groupon Master inside Pages & Posts without the need of extra plugins or gimmicks. Fast page load times and top SEO. Only available in advanced version.</div>
+	<br>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Groupon Master Website</b>
+	</p>
+	<p><a class="button-secondary" href="http://wordpress.techgasp.com/groupon-master/" target="_blank" title="Groupon Master Info Page">Info Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/groupon-master-documentation/" target="_blank" title="Groupon Master Documentation">Documentation</a> <a class="button-primary" href="http://wordpress.org/plugins/groupon-master/" target="_blank" title="Groupon Master Wordpress">RATE US *****</a></p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Advanced Version Updater</b>
+	</p>
+	<div class="description">The advanced version updater allows to automatically update your advanced plugin. Only available in advanced version.</div>
+	<br>
+	<?php
+	}
+ }
+?>
---
+++ readme.txt
@@ -0,0 +1,134 @@
+=== Groupon Master ===
+Contributors: Techgasp
+Tags: wordpress, plugin, widget, shortcode, techgasp, groupon, affiliate, ads, income, sale, make money, revenue
+Requires at least: 3.5
+Tested up to: 3.8
+Stable tag: 4.1
+License: GPLv2 or later
+
+Easy to use bombastic plugin that will be a great source of income for any wordpress webmaster. Groupon Master.
+
+
+== Description ==
+
+One of our main concerns at TechGasp is to find solid ways for wordpress based websites to generate revenue. For that we already launched professional plugins like Google Ads Master or Amazon Master.
+
+Now it's time to tap into big bucks with the famous and acclaimed Groupon network. Groupon is a website that features deals-of-the-day in major cities with excellent prices.
+
+Groupon Master lets you integrate Groupon Deals inside your wordpress blog. It was never this easy to share and sell those deals with your users and visitors. Groupon Master automatically displays this ads and generates lot's of cash.
+
+The plugin is totally customizable within the Groupon API, that means it fits perfectly into any of your template widget positions. With more than 15 ad sizes and side deals, it also packs groupon api accent color for the ads.
+
+That's not all, you can even specify a city to show ads from or just have the cities in auto-detect according to the origin of your wordpress visitor.
+
+Main features: Solid Revenue for Wordpress - Uses Groupon API (select any of the major cities supported by Groupon to show ads from or have it in Auto-Detect visitor city) - API ad sizes - API city selector - API deals - Minimal code, fast load times, error free, no conflicts - Google SEO Ready - Shortcode framework v2 to publish ads inside pages and posts - Easy to use, documentation available
+
+**solid revenue for wordpress...**
+
+IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX
+
+**LITE Version Contains:**
+
+* Display or hide Widget Title
+* Display or hide Groupon Ads
+* Insert desired groupon ad (allows api ads styling)
+
+**ADVANCED Version Contains:**
+
+* Contains the extra Shortcode Framework v2. Publish plugin inside pages and posts to double your income.
+* No need for extra shortcode plugins that will just make your website slow. Fast page load times and Google SEO ready.
+* [Advanced Version Link](http://wordpress.techgasp.com/groupon-master/)
+
+**ONLINE DEMO**
+
+* Right of the page, sidebar widget
+* Inside the post, with shortcode framework v2
+* [Demo Link](http://wordpress.techgasp.com/groupon-master)
+
+**More High Quality Plugins?**
+
+* For professional wordpress websites
+* Tested for fast page load times and SEO
+* Errors and conflicts free. NO JAVASCRIPT or AJAX
+* [Click Here](http://wordpress.techgasp.com/buy-all-techgasp-wordpress-plugins/)
+
+
+== Installation ==
+
+1. Use the Wordpress Installer, Updater.
+2. Activate in the "Plugins" admin panel.
+3. Visit the "Widget Page | Groupon Master" to configure the settings and your template widget position.
+4. [Groupon Master Documentation](http://wordpress.techgasp.com/groupon-master-documentation/)
+
+
+== Frequently Asked Questions ==
+
+= Issues with Ads display =
+* Please update to latest version. Up-to-date API's are available
+
+= Where is Shortcode? =
+* Advanced Version contains all plugin options and the Shortcode Framework v2, publish widget inside pages and posts
+* [Advanced Version Link](http://wordpress.techgasp.com/groupon-master/)
+* Insert the code into the page or post. [groupon-master]
+
+= What's this Shortcode Framework v2 =
+* It's a framework developed by TechGasp and built upon wordpress marvellous resources.
+* Allows users to quickly and easily display plugins or widgets inside their pages and posts.
+* It's available in all TechGasp advanced version plugins. Lite versions plugins do not include shortcode framework v2.
+* [About Shortcode Framework v2](http://wordpress.techgasp.com/new-wordpress-framework-from-techgasp/)
+
+= Need More Help =
+* [Groupon Master Documentation](http://wordpress.techgasp.com/groupo-master-documentation)
+* [Support](http://wordpress.techgasp.com/support)
+
+
+== Screenshots ==
+
+1. Widget With Title + Groupon Deals Small Vertical Banner.
+2. Widget Without Title, Groupon Deals Horizontal Banner.
+3. Widget Without Title, Groupon Deals Small Square Ad.
+4. Widget Without Title + Groupon Deals Big Vertical Banner.
+5. Widget inside pages and posts. Shortcode framework implementations. Groupon Deals Square Ad.
+
+
+== Changelog ==
+
+= 4.1 =
+* Minor tweaks related to new stock buttons
+
+= 4.0 =
+* Stable Release
+* Faster Background Updater
+* Wordpress 3.8 Ready
+
+= 3.0 =
+* Implementing advanced updater
+
+= 2.0 =
+* Release Candidate
+* Adding Shortcode Framework, advanced version only
+* Adding more screenshots
+
+= 1.5 =
+* Beta release
+* Adding Groupon API
+* Fixing warnings and other stuff
+
+= 1.0 =
+* Alpha release
+* Start coding
+
+
+== Upgrade Notice ==
+
+= 4.0 =
+* IMPORTANT Upgrade to new TechGasp Plugin Framework
+
+= 2.0 =
+* IMPORTANT Upgrade to Stable release
+
+= 1.5 =
+* IMPORTANT Beta Release, if you helped us test the Plugin, upgrade to Stable release
+
+= 1.0 =
+* IMPORTANT Alpha Release, if you helped us test the Plugin, upgrade to Beta
